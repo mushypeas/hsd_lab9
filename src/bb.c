b6af448c2aeb58a3cf28b6ad3bb56db025c64fb3
mushypeas <qkrwoans2@gmail.com>
2021-05-19 22:05:50 +0900

    d

--- src/bb.c
+++ src/bb.c
@@ -1,42 +1,40 @@
 #include<stdio.h>    
 #include<stdlib.h>  
 int main(){  
-int a[16][16],b[16][16],mul[16][16],r,c,i,j,k;    
-system("cls");  
-printf("enter the number of row=");    
-scanf("%d",&r);    
-printf("enter the number of column=");    
-scanf("%d",&c);    
-printf("enter the first matrix element=\n");    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-scanf("%d",&a[i][j]);    
-}    
+  int num_output = 16;
+  int num_input = 8;
+  int num_matrix2 = 8   ;
+int a[num_output][num_input],b[num_input][num_matrix2],mul[num_output][num_matrix2],i,j,k;    
+printf("enter the first matrix =\n");    
+for(i=0;i<num_output;i++)    
+  {    
+  for(j=0;j<num_input;j++)    
+  {    
+  scanf("%d",&a[i][j]);    
+  }    
 }    
 printf("enter the second matrix element=\n");    
-for(i=0;i<r;i++)    
+for(i=0;i<num_input;i++)    
 {    
-for(j=0;j<c;j++)    
+for(j=0;j<num_matrix2;j++)    
 {    
 scanf("%d",&b[i][j]);    
 }    
 }    
     
 printf("multiply of the matrix=\n");    
-for(i=0;i<16;i++) {    
-  for(j=0;j<16;j++) {    
+for(i=0;i<num_output;i++) {    
+  for(j=0;j<num_matrix2;j++) {    
     mul[i][j]=0;    
-    for(k=0;k<16;k++){    
+    for(k=0;k<num_input;k++){    
       mul[i][j]+=a[i][k]*b[k][j];    
     }    
   }    
 }    
 //for printing result    
-for(i=0;i<r;i++)    
+for(i=0;i<num_output;i++)    
 {    
-for(j=0;j<c;j++)    
+for(j=0;j<num_matrix2;j++)    
 {    
 printf("%d\t",mul[i][j]);    
 }    
